fbcd175848aac6cdb5bf6d8e0648b85b212dc0f3
artem.denisov <artem.denisov@devexpress.com>
2026-03-20 15:49:40 +0400

    update

--- WorkflowSample/ResultPrinter.cs
+++ WorkflowSample/ResultPrinter.cs
@@ -4,8 +4,9 @@ public static class ResultPrinter {
     public static void Print(string result) {
         var formatedResult = result.Replace("\n", Environment.NewLine);
 
-        Console.WriteLine("\u001b[32m=== GENERATED REPORT ===\u001b[0m");
-        Console.WriteLine("\u001b[33m" + formatedResult + "\u001b[0m");
-        Console.WriteLine("\u001b[32m=== FINISH ===\u001b[0m");
+        Console.WriteLine($"{AnsiColors.Green}++++++++++++++++++++++++++++++++++++++{AnsiColors.Reset}");
+        Console.WriteLine($"{AnsiColors.Green}GENERATED REPORT:{AnsiColors.Reset}");
+        Console.WriteLine();
+        Console.WriteLine($"{AnsiColors.BrightYellow}{formatedResult}{AnsiColors.Reset}");
     }
 }
